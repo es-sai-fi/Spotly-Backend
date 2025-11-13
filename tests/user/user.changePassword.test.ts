deb9fe04bffe532f8d2042c4b4b82417c96864ae
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-12 22:35:19 -0500

    Rename changePassword method to changeUserPassword

--- tests/user/user.changePassword.test.ts
+++ tests/user/user.changePassword.test.ts
@@ -113,7 +113,7 @@ describe("Integration - changePasswordController", () => {
       .spyOn(userService, "getUserById")
       .mockResolvedValueOnce(mockUser as any);
     const spyChange = jest
-      .spyOn(userService, "changePassword")
+      .spyOn(userService, "changeUserPassword")
       .mockResolvedValueOnce(true as any);
 
     const res = await request(app)
