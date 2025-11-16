cc51a9cd359acf467fc62faeaacda4fb7059ecad
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-16 16:30:11 -0500

    Delete src/models directory
    Outdated sql models that don't represent the actual models in supabase therefore not needed.

--- src/models/posts.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE posts (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  business_id UUID NOT NULL,
-  content TEXT NOT NULL,
-  likes INTEGER NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  FOREIGN KEY (business_id) REFERENCES businesses(id) ON DELETE CASCADE
-);
